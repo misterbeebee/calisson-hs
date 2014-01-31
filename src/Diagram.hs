3a5e771d7c6b12bb2c4161745025c33562ac4a08
misterbee <fpcomplete@mrbb.org>
2014-01-31 08:07:47 +0000

    color triangles by distance to home corner. first valid (calisson-compatible) coloring!

--- src/Diagram.hs
+++ src/Diagram.hs
@@ -6,7 +6,6 @@ module Diagram(diagram) where
 import Math((/.), (**.), cartesianProduct)
 import Color(colorScale, cie)
 import DiagramLib
-import Data.List
 import Diagrams.Backend.SVG
 import Diagrams.Core.Style
 import Diagrams.Color.HSV
@@ -21,17 +20,17 @@ import Diagrams.TwoD.Combinators
 import Data.Colour.RGBSpace
 import Data.Colour.SRGB.Linear
 import Data.Default
+import Data.List
+import Data.Map((!))
+import qualified Data.Map as M
 
-gridSize = 1
+gridSize = 5
 rows = 4*gridSize-1
 maxCols = 2*gridSize
 
 -- Needs Renderable Text and Renderable Path, so just hardcode SVG
 diagram :: QD SVG
 diagram = 
-   withName (2::Int,3::Int) (recolor white) .
-   withName (2::Int,3::Int) (recolor blue) .
-   withName (2::Int,3::Int) (recolor orange) .
     frame .
     colorize .
     vcatSnug $   
@@ -39,46 +38,81 @@ diagram =
        (\(row, (parity, cols)) -> hcatSnug $
                 -- add name for indexing
                 -- add label as visual aid 
-                zipWith (\col tri -> named (toName (row, col)) (mkLabel row col <> tri)) [(0::Int)..]                
+                zipWith (\col tri -> named (toName (row, col)) (mkLabel row col <> tri)) 
+                        (fenceposts cols)
                 . take cols . drop parity . cycle  -- a row in a large triangle
                 $ fmap ($ triangle 1) [rotateBy (1/4), rotateBy (-1/4)]
        ) gridList
   where rows = length gridList
 
 
-gridList = zip [(0::Int)..] $ concat [
+-- coordinates of centers of triangles (`fencepost` is misnomer)
+fenceposts :: Int -> [Double]
+fenceposts length =
+    fmap
+        (\x -> fromIntegral x - (fromIntegral length - 1) / 2)
+        [0..length-1]
+
+gridList = zip (fenceposts rows) $ concat [
   zip (repeat 0) (fmap (\x -> 2*x) [1..gridSize]),
   zip (cycle [1,0]) (replicate (2*gridSize-1) (2*gridSize)),
   zip (repeat 0) (fmap (\x -> 2*x) (reverse [1..gridSize]))
   ]
 
-mkLabel row col = scale 0.2 . text $ shows row (',' : show col)
+-- turn down opacity when not debuggin
+mkLabel row col = scale 0.2 . opacity 0.2 text $ shows (round $ 2*row) (',' : show (round $ 2*col))
 
 -- fixme
 recolor :: Colour Double -> ModifyFn
-recolor c cell dia = (getSub cell # fc c # opacity 0.5)  <> dia 
+recolor c cell dia = (getSub cell # fc c # opacity 0.7)  <> dia 
 
 
-cellLabels :: [(Int,Int)]
-cellLabels = cartesianProduct [0..rows] [0..maxCols]
+cellLabels :: [(Double,Double)]
+cellLabels = cartesianProduct (fenceposts rows) (fenceposts maxCols)
 
 -- fixme: awful hack! copy each named subdiagram (which fortunately does not copy the name),
 -- and put a colored version `atop` it
 colorize :: QDTrans b 
-colorize = modifyByName
-            (\name sub dia -> colorize1 (rows, maxCols) name (getSub sub) <> dia)
-            cellLabels
+colorize = modifyByName modifyFn cellLabels
+               where modifyFn name = recolor (getColor (rows, maxCols) name)
 
 modifyByName :: (BR2 b, IsName n) => (n -> Subdiagram b R2 Any -> QD b -> QD b) -> [n] -> QDTrans b 
 modifyByName modifyFn names d = foldl' (\d name -> withName name (modifyFn name) d) d names
             
---colorize1 _ _ = id
-colorize1 :: (BR2 b) => (Int,Int) -> (Int,Int)  -> QDTrans b 
-colorize1 = colorizeArb
+getColor :: (Int,Int) -> (Double,Double)  -> Colour Double
+getColor = getHomeColor
 
 -- distribute three colors arbitrarily
-colorizeArb :: (BR2 b) => (Int,Int) -> (Int,Int)  -> QDTrans b 
-colorizeArb (rows,cols) (row, col) =
-        let n = ((((2**.col * 3**.row) `mod` 259) **. 10) - 1) `mod` 3 in
-                  fc ([red,green,purple] !! n)
 
+homeColors :: M.Map (Double, Double) (Colour Double)
+homeColors = let s = fromIntegral gridSize in
+  M.fromList [
+          ((0.5-s*2, 0) , red)
+        , ((s, -s), green) -- todo, use `rows instead of `size` ?
+        , ((s, s), blue)
+        ]
+
+getHomeColor :: (Int,Int) -> (Double,Double) -> Colour Double  
+getHomeColor _ cell@(row, col) = 
+    let fcell = cell in
+    snd $ foldl1WithKey
+        (\(nearest, ncolor) corner ccolor ->  
+            -- fixme ugh
+            if dist fcell corner < dist fcell nearest
+            then (corner, ccolor)
+            else (nearest, ncolor))
+        homeColors
+
+
+dist a b = norm (a-b)
+
+norm :: (Double, Double) -> Double
+norm (x,y) = abs(x) + abs(y)
+
+sqr x = x * x
+
+toF :: (Int, Int) -> (Double, Double)
+toF (a,b) = (fromIntegral a, fromIntegral b)
+
+foldl1WithKey :: ((k,v) -> k -> v -> (k,v)) -> M.Map k v -> (k,v)
+foldl1WithKey f m =  M.foldlWithKey f (head . M.toList $ m) m
